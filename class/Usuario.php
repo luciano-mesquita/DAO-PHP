60ae75bf883f6ad480db6dd2d3008f69e651c8f7
Luciano Mesquita <lucianomesquita01@gmail.com>
2020-06-27 12:48:39 -0300

    Implementando novos metodos na classe usuário

--- class/Usuario.php
+++ class/Usuario.php
@@ -39,6 +39,7 @@ class Usuario {
 		$this->senha = $senha;
 	}
 
+	// Função que pega os dados de um usuário no banco e carrega a classe
 	public function loadById($id) {
 
 		$banco = new Banco();
@@ -58,6 +59,50 @@ class Usuario {
 		}
 	}
 
+	// Retorna todos os usuários do banco
+	public static function getList() {
+
+		$banco = new Banco;
+
+		return $banco->select("SELECT * FROM usuario");
+	}
+
+	// Retorna um usuário, filtrando pelo EMAIL
+	public static function search($email) {
+
+		$banco = new Banco;
+
+		return $banco->select("SELECT * FROM usuario WHERE email LIKE :SEARCH ORDER BY email", array(
+			':SEARCH'=>"%".$email."%"
+		));
+	}
+
+	// Retorna os dados verificando o login e senha
+	public function login($email, $senha) {
+
+		$banco = new Banco();
+
+		$results = $banco->select("SELECT * FROM usuario WHERE email = :EMAIL AND senha = :SENHA", array(
+			":EMAIL"=>$email,
+			":SENHA"=>$senha
+		));
+
+		if (count($results) > 0) {
+			
+			$row = $results[0];
+
+			$this->setId($row['id']);
+			$this->setNome($row['nome']);
+			$this->setEmail($row['email']);
+			$this->setSenha($row['senha']);
+		
+		} else {
+
+			throw new Exception("Login e/ou senha inválidos");	
+		}
+	}
+
+	// Retorna um JSON com os dados de um determinado usuário
 	public function __toString() {
 
 		$dados = json_encode(array(
--- index.php
+++ index.php
@@ -1,13 +1,22 @@
 <?php
 
-// Carregando o arquivo de configuração
 require_once("config.php");
 
-// Criando um usuário
-$usuario1 = new Usuario();
+// Lista apenas um usuário
+//$usuario1 = new Usuario();
+//$usuario1->loadById(5);
+//echo $usuario1;
 
-// Utilizando um método para carregar os dados do usuário
-$usuario1->loadById(5);
+// Lista todos os usuários
+//$lista = Usuario::getList();
+//echo json_encode($lista);
 
-// Exibindo os dados carregados
-echo $usuario1;
+// Carrega uma lista de usuário buscando pelo email
+//$search = Usuario::search("ad");
+//echo json_encode($search);
+
+// Carrega um usuário usando o login e a senha
+$usuario = new Usuario();
+$usuario->login("admin@admin.com", "1234567890");
+
+echo $usuario;
